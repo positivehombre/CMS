b28d4955be24ef46ac9f20cc0556dca86752ca4b
positivehombre <furki168@hotmail.com>
2020-09-15 17:32:43 +0300

    Toggle button eklendi. Yeni ekle butonu eklendi. Kayıt yoktur alert paneli eklendi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,13 +1,20 @@
 <div class="row">
 	
 	<div class="col-md-12">
-		<h4 class="m-b-lg"> Ürünler </h4>
+		<h4 class="m-b-lg"> 
+			Ürün Listesi 
+			<a href="#" class="btn btn-outline btn-primary btn-xs pull-right"><i class="fa fa-plus"></i> Yeni Ekle </a>
+		</h4>
 	</div>
 
 	<div class="col-md-12">
 		<div class="widget p-lg">
+
+			<div class="alert alert-info text-center">
+				<p>Herhangi bir kayıt bulunamadı <a href="#"> eklemek için tıklayınız </a></p>
+			</div>
+
 			<table class="table table-hover table-striped">
-				
 				<thead>
 					<th>#ID</th>
 					<th>url</th>
@@ -23,7 +30,53 @@
 						<td>monitor-askisi</td>
 						<td>Monitör Askısı</td>
 						<td>360 derece kullanılabilen monitör askısı</td>
-						<td>1/0</td>
+						<td>
+							<input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+						</td>
+						<td>
+							<button type="button" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </button>
+							<button type="button" class="btn btn-sm btn-primary btn-outline"><i class="fa fa-edit"></i> Düzenle </button>
+						</td>
+					</tr>
+
+
+					<tr>
+						<td>#1</td>
+						<td>monitor-askisi</td>
+						<td>Monitör Askısı</td>
+						<td>360 derece kullanılabilen monitör askısı</td>
+						<td>
+							<input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+						</td>
+						<td>
+							<button type="button" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </button>
+							<button type="button" class="btn btn-sm btn-primary btn-outline"><i class="fa fa-edit"></i> Düzenle </button>
+						</td>
+					</tr>
+
+					<tr>
+						<td>#1</td>
+						<td>monitor-askisi</td>
+						<td>Monitör Askısı</td>
+						<td>360 derece kullanılabilen monitör askısı</td>
+						<td>
+							<input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+						</td>
+						<td>
+							<button type="button" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </button>
+							<button type="button" class="btn btn-sm btn-primary btn-outline"><i class="fa fa-edit"></i> Düzenle </button>
+						</td>
+					</tr>
+
+
+					<tr>
+						<td>#1</td>
+						<td>monitor-askisi</td>
+						<td>Monitör Askısı</td>
+						<td>360 derece kullanılabilen monitör askısı</td>
+						<td>
+							<input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+						</td>
 						<td>
 							<button type="button" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </button>
 							<button type="button" class="btn btn-sm btn-primary btn-outline"><i class="fa fa-edit"></i> Düzenle </button>
